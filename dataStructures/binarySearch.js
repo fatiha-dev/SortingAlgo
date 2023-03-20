343fc3914345f7cffccd342258df6971e6d7dbf1
fatiha-dev <tihaelbou@gmail.com>
2023-03-20 21:36:36 +0000

    Add binary search algo

---
+++ dataStructures/binarySearch.js
@@ -0,0 +1,27 @@
+const binarySearch = (array, target) => {
+    let startIndex = 0;
+    let endIndex = array.length - 1;
+    while(startIndex <= endIndex) {
+      let middleIndex = Math.floor((startIndex + endIndex) / 2);
+      if(target === array[middleIndex]) {
+        return console.log("Target was found at index " + middleIndex);
+      }
+      if(target > array[middleIndex]) {
+        console.log("Searching the right side of Array")
+        startIndex = middleIndex + 1;
+      }
+      if(target < array[middleIndex]) {
+        console.log("Searching the left side of array")
+        endIndex = middleIndex - 1;
+      }
+      else {
+        console.log("Not Found this loop iteration. Looping another iteration.")
+      }
+    }
+    
+    console.log("Target value not found in array");
+  }
+
+  const arr = [2,3,7,11,13,17,19,31]
+  const res = binarySearch(arr,31);
+  console.log(res);
